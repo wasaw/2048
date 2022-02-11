833349f14a4af294c8522e78bbf8920eceaa94bf
wasaw <merenkovalexander@gmail.com>
2022-02-11 16:32:09 +0300

    Initial Commit

---
+++ 2048/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  2048
+//
+//  Created by Александр Меренков on 2/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
